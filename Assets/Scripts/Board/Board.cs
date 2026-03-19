465261f08243e2247a170adac8f28a69e48f90f3
edu-23-amica <0264528@up.edu.mx>
2026-03-19 16:41:36 -0600

    Board with hexagonal shape

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -6,7 +6,11 @@ public class Board : MonoBehaviour
     [SerializeField] private int rows = 10;
 
     [SerializeField] private int columns = 6;
-    private float spaceBetweenTiles = 1f;
+    private float spaceBetweenTiles = 1.75f;
+    private float spaceBetweenRows = 1.5f;
+    private float rowOffset = 0f;
+
+    private float hightValue;
 
     void Start()
     {
@@ -25,21 +29,33 @@ public class Board : MonoBehaviour
 
         for (short x=0; x<rows; x++)
         {
-            for(short z=0; z<columns; z++)
+            if(x % 2 != 0)
             {
-                positionOfTiles = new Vector3(x * spaceBetweenTiles, 0, z * spaceBetweenTiles);
-                tileInstance = Instantiate(tilePrefab, positionOfTiles, Quaternion.identity);
+                columns--;
+                rowOffset = spaceBetweenTiles / 2;
+            }
+            else
+            {
+                columns++;
+                rowOffset = 0f;
+            }
 
-                Tile tile = tileInstance.GetComponent<Tile>();
+                for (short z = 0; z < columns; z++)
+                {
+                hightValue = Random.Range(0f, 0.5f);
+                positionOfTiles = new Vector3(x * spaceBetweenRows, hightValue, z * spaceBetweenTiles + rowOffset);
+                    tileInstance = Instantiate(tilePrefab, positionOfTiles, Quaternion.Euler(90f, 0f, -90f)); //Quaternion.identity)
+                    Tile tile = tileInstance.GetComponent<Tile>();
 
-                if ((x + z) % 2 == 0)
-                {
-                    tile.SetColor(Color.white);
-                }
-                else
-                {
-                    tile.SetColor(Color.black);
-                }
+
+                    if ((x + z) % 2 == 0)
+                    {
+                        tile.SetColor(Color.white);
+                    }
+                    else
+                    {
+                        tile.SetColor(Color.black);
+                    }
             }
         }
     }
