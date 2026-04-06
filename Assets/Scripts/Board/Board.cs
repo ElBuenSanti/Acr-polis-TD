8441ecd4e12cb958c24c539d2029099eae3e50cd
edu-23-amica <0264528@up.edu.mx>
2026-04-06 13:56:57 -0600

    NavMesh component

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.AI; //
 
 public class Board : MonoBehaviour
 {
     [SerializeField] private GameObject tilePrefab;
     [SerializeField] private int rows = 10;
-
     [SerializeField] private int columns = 6;
+
+    [SerializeField] private NavMeshSurface navSurface; //
+    private string navMeshObject = "NavMeshSurfaceBoard"; //
+    private string layerMaskName = "Tiles"; //
+
     private float spaceBetweenTiles = 1.75f;
     private float spaceBetweenRows = 1.5f;
     private float rowOffset = 0f;
@@ -14,37 +19,39 @@ public class Board : MonoBehaviour
 
     void Start()
     {
+
+        GameObject navObj = new GameObject(navMeshObject);
+        navSurface = navObj.AddComponent<NavMeshSurface>();
+        navSurface.collectObjects = CollectObjects.All;
+        navSurface.layerMask = LayerMask.GetMask(layerMaskName); 
+
         GenerateBoard();
+        navSurface.BuildNavMesh(); //
     }
-
-    void Update()
-    {
-        
-    }
-
     void GenerateBoard()
     {
         Vector3 positionOfTiles;
         GameObject tileInstance;
+        int currentColumns = columns;
 
         for (short x=0; x<rows; x++)
         {
             if(x % 2 != 0)
             {
-                columns--;
+                currentColumns--; //columns--;
                 rowOffset = spaceBetweenTiles / 2;
             }
             else
             {
-                columns++;
+                currentColumns = columns; // columns++;
                 rowOffset = 0f;
             }
 
-                for (short z = 0; z < columns; z++)
+                for (short z = 0; z < currentColumns; z++) //columns
                 {
                     hightValue = Random.Range(0f, 0.5f);
-                    positionOfTiles = new Vector3(x * spaceBetweenRows, hightValue, z * spaceBetweenTiles + rowOffset);
-                    tileInstance = Instantiate(tilePrefab, positionOfTiles, Quaternion.Euler(90f, 0f, -90f)); 
+                    positionOfTiles = new Vector3(x * spaceBetweenRows, 0, z * spaceBetweenTiles + rowOffset); //y=hightValue
+                tileInstance = Instantiate(tilePrefab, positionOfTiles, Quaternion.Euler(90f, 0f, -90f)); 
                     Tile tile = tileInstance.GetComponent<Tile>();
 
 
